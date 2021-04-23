d29f2c5733e5952c6884cf61af6805dbd1606f66
taler0n <leonchik.vin@mail.ru>
2021-04-23 11:33:54 +0300

    Добавьте файлы проекта.

---
+++ BabaiAlg/Babai.cs
@@ -0,0 +1,51 @@
+﻿using CryptoAlg;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BabaiAlg
+{
+    public class Babai
+    {
+        public static Vector FindClosest(Matrix basis, Vector x)
+        {
+            var reducedBasis = LLL.LLLreduction(basis);
+            Console.WriteLine("LLL-редуцированный базис:");
+            reducedBasis.ConsolePrint();
+            Console.WriteLine();
+
+            Matrix gramVectors;
+            Matrix gramCoef;
+            LLL.GramSchmidt(basis, out gramVectors, out gramCoef);
+            Console.WriteLine("Ортогонализированные векторы:");
+            gramVectors.ConsolePrint();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var xi = new Vector(x);
+            var closest = new Vector(x.Length);
+            for (int i = x.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine("Шаг {0}", x.Length - i);
+                Console.WriteLine("Вектор x{0}:", i + 1);
+                xi.ConsolePrint();
+                Console.WriteLine();
+
+                double r = xi.DotProduct(gramVectors[i]) / gramVectors[i].DotProduct(gramVectors[i]);
+                int m = Convert.ToInt32(Math.Floor(r + 0.5));
+                Console.WriteLine("r = {0}; m = {1}", r, m);
+                Console.WriteLine();
+
+                var y = m * reducedBasis[i];
+                Console.WriteLine("Вектор y:");
+                y.ConsolePrint();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                closest = closest + y;
+                xi = xi + ((m - r) * gramVectors[i]) - y;
+            }
+            return closest;
+        }
+    }
+}
---
+++ BabaiAlg/Program.cs
@@ -0,0 +1,28 @@
+﻿using CryptoAlg;
+using System;
+
+namespace BabaiAlg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basis1 = new Matrix(new double[,] { { 1, 173, -220 }, { 0, 547, 0 }, { 0, 0, 547 } });
+            var x1 = new Vector(new double[] { 33, 34, 35 });
+            var basis2 = new Matrix(new double[,] { { 1, 2, 3 }, { 3, 0, -3 }, { 3, -7, 3 } });
+            var x2 = new Vector(new double[] { 10, 6, 5 });
+
+            var basis = basis2;
+            var x = x2;
+            Console.WriteLine("Базис решетки:");
+            basis.ConsolePrint();
+            Console.WriteLine();
+            Console.WriteLine("Проверяемая точка:");
+            x.ConsolePrint();
+            Console.WriteLine();
+            var closest = Babai.FindClosest(basis, x);
+            Console.WriteLine("Результат алгоритма:");
+            closest.ConsolePrint();
+        }
+    }
+}
